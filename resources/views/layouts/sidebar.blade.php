b4d1edf06781f27bd8cbb0795d5ddb8fa8ff19da
asrani <asrani.27@gmail.com>
2024-10-07 13:01:20 +0800

    f

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,10 +10,10 @@
     <li class="{{ (request()->is('superadmin/petugas*')) ? 'active' : '' }}"><a href="/superadmin/petugas"><i class="fa fa-users"></i> <span>Data petugas</span></a></li>
     <li class="{{ (request()->is('superadmin/kategori*')) ? 'active' : '' }}"><a href="/superadmin/kategori"><i class="fa fa-list"></i> <span>Data Kategori</span></a></li>
     <li class="{{ (request()->is('superadmin/cagar*')) ? 'active' : '' }}"><a href="/superadmin/cagar"><i class="fa fa-building"></i> <span>Data Cagar Budaya</span></a></li>
-    <li class="{{ (request()->is('superadmin*')) ? 'active' : '' }}"><a href="/superadmin"><i class="fa fa-edit"></i> <span>Absensi</span></a></li>
+    <li class="{{ (request()->is('superadmin/absensi*')) ? 'active' : '' }}"><a href="/superadmin"><i class="fa fa-edit"></i> <span>Absensi</span></a></li>
     <li class="{{ (request()->is('superadmin/jadwal*')) ? 'active' : '' }}"><a href="/superadmin/jadwal"><i class="fa fa-eye"></i> <span>Jadwal Monitoring</span></a></li>
     
-    <li class="{{ (request()->is('superadmin*')) ? 'active' : '' }}"><a href="/superadmin"><i class="fa fa-file"></i> <span>Print Laporan</span></a></li>
+    <li class="{{ (request()->is('superadmin/laporan*')) ? 'active' : '' }}"><a href="/superadmin"><i class="fa fa-file"></i> <span>Print Laporan</span></a></li>
     <li><a href="/logout"><i class="fa fa-sign-out"></i> <span>Logout</span></a></li>
     @else
         
